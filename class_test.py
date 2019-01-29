da0afc8bf61d9dc30d08bd9b813c25389d5b5f76
David Hong <secuof@gmail.com>
2019-01-29 21:57:55 +0900

    class test again

--- class_test.py
+++ class_test.py
@@ -1,30 +1,29 @@
-class fourcal:
-    def setdata(self, first, second):
-        self.first = first
-        self.second = second
-    def sum(self):
-        result = self.first + self.second
-        return result
-    def mul(self):
-        result = self.first * self.second
-        return result
-    def div(self):
-        result = self.first / self.second
-        return result
-    def sub(self):
-        result = self.first / self.second
-        return result
-a = fourcal()
-b = fourcal()
-inputa = int(input())
-inputb = int(input())
-a.setdata(inputa, inputb)
-sum = a.sum()
-mul = a.mul()
-div = a.div()
-sub = a.sub()
-print ("SUM:" sum)
-print ("Multiplication:",mul)
-print ("Division:",div)
-print ("Subtraction:",sub)
+class honghouse:
+    lastname = "홍"
+    def __init__(self, name):
+        self.fullname = self.lastname + name
+    def travel(self, where):
+        print("%s, %s여행을 가다." % (self.fullname, where))
+    def love(self, other):
+        print("%s, %s 사랑에 빠졌네" % (self.fullname, other.fullname))
+    def fight(self, other):
+        print("%s, %s 싸우네" % (self.fullname, other.fullname))
+    def __add__(self, other):
+        print("%s, %s 결혼했네" % (self.fullname, other.fullname))
+    def __sub__(self, other):
+        print("%s, %s 이혼했네" % (self.fullname, other.fullname))
 
+
+class kimhouse(honghouse):
+    lastname = "김"
+    def travel(self, where, day):
+        print("%s, %s 여행 %d일 가네." % (self.fullname, where, day))
+
+me = honghouse("로미오")
+wife = kimhouse("줄리엣")
+me.travel("부산")
+wife.travel("부산", 3)
+me.love(wife)
+me.fight(wife)
+me + wife
+me - wife
