5ed230bc21e9810520fa88b75ba60ee3ae6d1357
David Hong <secuof@gmail.com>
2019-02-21 22:16:58 +0900

    test create dir

--- psycopg2_test/psycopg2_createdir.py
+++ psycopg2_test/psycopg2_createdir.py
@@ -1,27 +1,50 @@
 import psycopg2
 import os
 
-# Connect to the DB
-con = psycopg2.connect(
-    host = "192.168.1.226",
-    database = "clarity",
-    user = "clarity",
-    password = "clarity"
-)
 
-# Cursor
-cur = con.cursor()
+# # Connect to the DB
+# con = psycopg2.connect(
+#     host = "192.168.1.226",
+#     database = "clarity",
+#     user = "clarity",
+#     password = "clarity"
+# )
 
-# Excute query
-cur.execute("select package, version from processed limit 10")
+# # Cursor
+# cur = con.cursor()
 
-rows = cur.fetchall()
+# # Excute query and fetchall
+# cur.execute("select package, version, checksum from processed limit 10")
+# rows = cur.fetchall()
 
-for r in rows:
-    print(f"id : {r[0]} || name : {r[1]}")
+# for r in rows:
+#     print(f"id : {r[0]} || name : {r[1]} || test = {r[2]}")
 
-# Close the cursor
-cur.close()
+# # Close the cursor
+# cur.close()
+# # Close the connection
+# con.close()
 
-# Close the connection
-con.close()
+
+YOUR_DIRECTORY_NAME = "./test"
+os.makedirs(os.path.join({YOUR_DIRECTORY_NAME}))
+
+## Create Directory (https://cinema4dr12.tistory.com/1296)
+# try:
+#     if not(os.path.isdir({YOUR_DIRECTORY_NAME})):
+#         os.makedirs(os.path.join({YOUR_DIRECTORY_NAME}))
+# except OSError as e:
+#     if e.errno != errno.EEXIST:
+#         print("Failed to create directory!!!!!")
+#         raise
+
+# ## Create File
+# import os
+ 
+# filepath = os.path.join({YOUR_FILE_PATH}, "test.txt")
+# fid = open(filepath, "w")
+
+# if not os.path.isfile(filepath):
+#     fid.write("Text file creation example.")
+ 
+# fid.close()
